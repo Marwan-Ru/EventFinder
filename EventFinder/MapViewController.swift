4a9620a8e1a077780e96bf5bd97564ea3002e524
Leo-BERNARD38 <leobernard712@gmail.com>
2024-04-08 17:07:19 +0200

    map link

---
+++ EventFinder/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  EventFinder
+//
+//  Created by Leo Bernard on 08/04/2024.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
